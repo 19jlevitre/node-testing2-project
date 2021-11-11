05250f4e4b56dd95a19da1ab074c3990174fc4da
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-11 14:02:54 -0700

    data base is running and seeded

---
+++ data/seeds/add-pokemon.js
@@ -0,0 +1,14 @@
+const pokemon = [
+  { name: 'pikachu' },
+  { name: 'bulbasaur' },
+  { name: 'squirtle' },
+  { name: "charmander" }
+]
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('pokemon')
+    .truncate()
+    .then(function () {
+      return knex('pokemon').insert(pokemon);
+    });
+};
