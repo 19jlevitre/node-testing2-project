391c3522a37c42ce971029c22469c2135ec1b8d0
Jonathon LeVitre <jonathonlevitre@Jonathons-MacBook-Pro.local>
2021-11-11 14:19:47 -0700

    api working with crud ops

--- api/server.js
+++ api/server.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const server = express()
+const Pokemon = require('./pokemon/pokemon-model')
 server.use(express.json())
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "up" });
+  });
+
+  server.get("/pokemon", (req, res) => {
+    Pokemon.getAll()
+      .then(pokemon => {
+        res.status(200).json(pokemon);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  server.get("/pokemon/:id", async (req, res) => {
+    res.json(await Pokemon.getById(req.params.id))
+  });
+
+  server.post("/pokemon", async (req, res) => {
+    res.json(await Pokemon.insert(req.body))
+  });
+
+  server.delete("/pokemon/:id", async (req, res) => {
+    res.json(await Pokemon.remove(req.params.id))
+  });
+
 module.exports = server
